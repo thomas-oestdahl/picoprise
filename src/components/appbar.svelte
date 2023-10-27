172a976083cad367a6bfdcf3ca7a4316ea7ab238
thomas-ostdahl <thomas@321.no>
2023-10-27 15:12:21 +0200

    Add more to appbar

--- src/components/appbar.svelte
+++ src/components/appbar.svelte
@@ -1,19 +1,37 @@
 <script>
+  import { page } from "$app/stores";
+  $: currentRoute = $page.url.pathname;
 </script>
 
-<section class="navbar bg-base-200 mb-5 rounded-br-full">
-  <div class="container mx-auto pr-10 flex items-center justify-between h-16">
+<section class="navbar bg-base-200 rounded-br-full">
+  <div class="container mx-auto pr-10 flex items-center justify-between">
     <div class="flex flex-col items-start">
-      <p class="text-2xl font-bold font-serif">
+      <a class="text-2xl font-bold font-serif" href="/">
         <span class="text-primary">Pico</span>Prise
-      </p>
+      </a>
       <p class="text-sm">Leading in sauna innovation</p>
     </div>
     <div class="flex flex-col items-start">
       <p class="text-lg">
-        <i class="ph ph-user mr-3"></i>John Doe
+        <i class="ph ph-user mr-3" />John Doe
       </p>
       <p class="text-sm">customer</p>
     </div>
   </div>
 </section>
+<nav class="navbar tabs mb-5">
+  <div class="container mx-auto">
+    <a
+      class="tab tab-lifted {currentRoute === '/' ? 'tab-active' : ''}"
+      href="/">Home</a
+    >
+    <a
+      class="tab tab-lifted {currentRoute === '/order' ? 'tab-active' : ''}"
+      href="/order">Order</a
+    >
+    <a
+      class="tab tab-lifted {currentRoute === '/myorder' ? 'tab-active' : ''}"
+      href="/myorder">My order</a
+    >
+  </div>
+</nav>
--- src/routes/order/+page.svelte
+++ src/routes/order/+page.svelte
@@ -2,7 +2,13 @@
   import Textfield from "../../components/textfield.svelte";
 </script>
 
-<section class="py-10">
+<section>
+  <nav class="text-sm breadcrumbs py-10">
+    <ul>
+      <li><a href="/" class="text-primary">PicoPrise</a></li>
+      <li>Order</li>
+    </ul>
+  </nav>
   <h1 class="text-3xl font-serif font-bold text-secondary">Order</h1>
   <form class="flex flex-col gap-4 py-2">
     <Textfield label="Fullname" placeholder="John Doe" type="text" />
